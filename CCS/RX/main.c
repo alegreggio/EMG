7e17cbf2a5a42d4543cdf6276aa069b1dcd7eb5e
chalter <halter.cd@gmail.com>
2016-08-28 19:48:12 -0300

    Inicie el main

--- CCS/RX/main.c
+++ CCS/RX/main.c
@@ -5,13 +5,24 @@
  */
 int main(void) {
 
+	uint8_t dato;
+
 	conf_WDT      	();                // Configura WDT del sistema
 	conf_CLK      	();                // Configura CLK del sistema
 	conf_IO       	();                // Configura Entradas/Salidas
 	conf_USI   		();                // Configura USCI_B0 --> SPI
 	nRF24L01_init 	();				   // Configura nRF24L01+
-	conf_TA0		();				   // Configura TimerA TA0
 	//P1OUT	^=	 BIT0;
+	while (1)
+	{
+		CE_HIGH;
+		while (!RX_DR);
+		CE_LOW;
+		spi_transfer (R_RX_PAYLOAD);
+
+
+
+	}
 
 	
 	return 0;
